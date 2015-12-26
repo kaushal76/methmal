3b99a598f4ff767cf9845b3fb8f59e4e032d963c
kaushal76 <kaushal.keraminiyage@gmail.com>
2015-12-26 16:26:08 +0000

    Minor formatting changes

--- components/com_confmgt/views/loginform/view.html.php
+++ components/com_confmgt/views/loginform/view.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     2.5.8.1
+ * @version     3.4.1
  * @package     com_confmgt
  * @copyright   Copyright (C) 2015. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
